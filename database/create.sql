4c05ca3c824d2de44e939ba0f0854afd27b1d1e2
11919159 <e11919159@student.tuwien.ac.at>
2021-12-20 13:52:53 +0100

    initial commit

---
+++ database/create.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS rooms (
+    id SERIAL,
+    name VARCHAR(255) NOT NULL,
+    building VARCHAR(127) NOT NULL,
+    area VARCHAR(127) NOT NULL,
+    region VARCHAR(127) NOT NULL,
+    floor VARCHAR(63) NOT NULL,
+    room_number VARCHAR(63) NOT NULL,
+    capacity INTEGER,
+    room_type VARCHAR(63) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS booking (
+    id SERIAL,
+    name VARCHAR(255) NOT NULL,
+    time TIMESTAMP NOT NULL,
+    room_id INTEGER NOT NULL,
+    CONSTRAINT fk_room FOREIGN KEY(room_id) REFERENCES rooms(id),
+    PRIMARY KEY(id)
+);
