a3faff25e585e77397598483d4e73ae1891dad8d
Benedict Armstrong <contact@benarmstro.ng>
2022-01-09 17:29:24 +0100

    deleted near me; updated deploy script

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,3 +1,4 @@
+npm run build
 docker build --platform linux/amd64 . -t benarmstrong/eth_rooms_frontend:latest
 docker push benarmstrong/eth_rooms_frontend:latest
-ssh ben-docker-1 "cd eth_rooms; docker-compose pull; docker-compose up -d"
+ssh ben-docker-1 "cd eth_rooms; docker-compose pull client; docker-compose up -d"
