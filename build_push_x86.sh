933bde9245a460f4c1e75e7295839b43661e0f4b
Benedict Armstrong <contact@benarmstro.ng>
2025-02-26 14:18:24 +0100

    build changes

--- build_push_x86.sh
+++ build_push_x86.sh
@@ -1,5 +1,2 @@
 docker build ./app -t benarmstrong/eth_rooms_client --platform linux/amd64
-docker build ./database -t benarmstrong/eth_rooms_database --platform linux/amd64
-
 docker push benarmstrong/eth_rooms_client
-docker push benarmstrong/eth_rooms_database
